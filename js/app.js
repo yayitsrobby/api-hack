d54d5fbfa663890e6e90744d89b250d0a8b4c594
Joe Seago <joe.seago@gmail.com>
2016-06-23 14:36:50 -0600

    finished event listeners

--- js/app.js
+++ js/app.js
@@ -1,36 +1,69 @@
 // TODO: write out the functions without code in them
 
-$(document).ready(function () {
+$(document).ready(function() {
 
 
-  // will populate the html elements and return html
-  function showRecs() {
 
-  }
+    // will populate the html elements and return html
+    function showRecs() {
 
-  // will query the api and receive the object and print
-  // the html
-  function getMoodRecs(mood) {
-    // showRecs();
-  }
+    }
 
-  // will take whatever tags and query the api and receive the
-  // object and print the html
-  function getClassicRecs(tags) {
-    // showRecs();
-  }
+    // will query the api and receive the object and print
+    // the html
+    function getMoodRecs(mood) {
+        $.ajax({
+            method: 'GET',
+            url: '',
+            dataType: 'json',
+            data: {
 
-  // listen to button click for which method
-  // if one than hide this and show that
-  // else, the other
+            }
+        }).done(function(mood) {
 
-  // listen for drop down
-  // clear last .mood-recs
-  // take the value given and pass it into a function that will query
-  // output the html elements - append to .mood-recs
+            showRecs();
+        });
+    };
 
-  // listen for submit button
-  // clear last .classic-recs
-  // take the value given and pass it into a function that will query
-  // output the html elements - append to .classic-recs
+    // will take whatever tags and query the api and receive the
+    // object and print the html
+    function getClassicRecs(tags) {
+        // showRecs();
+    }
+
+    // listen to button click for which method
+    $('.mood-button').on('click', function() {
+        event.preventDefault();
+        $('.splash-page').hide();
+        $('.mood-section').show();
+    });
+
+    $('.classic-button').on('click', function() {
+        event.preventDefault();
+        $('.splash-page').hide();
+        $('.classic-section').show();
+    });
+
+    // listen for Mood dropdown
+    $('.mood-choice').change(function() {
+        $('.mood-recs').empty();
+        var selectedMood = $(this).find(':selected').val();
+        getMoodRecs(selectedMood);
+    });
+
+    // listen for Classic Search submit button
+    $('.search-submit').on('click', function() {
+        event.preventDefault();
+        $('.classic-section').empty();
+        var searchQuery = $(this).val();
+        getClassicRecs(searchQuery);
+    });
+
+    // take the value given and pass it into a function that will query
+    // output the html elements - append to .mood-recs
+
+    // listen for submit button
+    // clear last .classic-recs
+    // take the value given and pass it into a function that will query
+    // output the html elements - append to .classic-recs
 });
