11fcc05d9bbd623bcade48c2b9a03e80da89a441
Robby Kim <robert.st.kim@gmail.com>
2016-06-23 15:45:56 -0400

    wrote out psuedo code and function names

---
+++ js/app.js
@@ -0,0 +1,36 @@
+// TODO: write out the functions without code in them
+
+$(document).ready(function () {
+
+
+  // will populate the html elements and return html
+  function showRecs() {
+
+  }
+
+  // will query the api and receive the object and print
+  // the html
+  function getMoodRecs(mood) {
+    // showRecs();
+  }
+
+  // will take whatever tags and query the api and receive the
+  // object and print the html
+  function getClassicRecs(tags) {
+    // showRecs();
+  }
+
+  // listen to button click for which method
+  // if one than hide this and show that
+  // else, the other
+
+  // listen for drop down
+  // clear last .mood-recs
+  // take the value given and pass it into a function that will query
+  // output the html elements - append to .mood-recs
+
+  // listen for submit button
+  // clear last .classic-recs
+  // take the value given and pass it into a function that will query
+  // output the html elements - append to .classic-recs
+});
