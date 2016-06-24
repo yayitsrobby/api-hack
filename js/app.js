097920877c6efd6902169f5de2ff546ecd96a64d
Joe Seago <joe.seago@gmail.com>
2016-06-24 12:01:40 -0600

    API pull

--- js/app.js
+++ js/app.js
@@ -1,83 +1,87 @@
 $(document).ready(function () {
-  var sadGenres = ['drama', 'tv movie', 'family'];
-  var happyGenres = ['adventure', 'animation', 'comedy', 'family', 'fantasy'];
-  var somberGenres = ['crime', 'documentary', 'drama', 'mystery', 'thriller', 'war'];
-  var sappyGenres = ['romance', 'tv movie', 'comedy', 'drama', 'family'];
-  var scaredGenres = ['horror', 'mystery', 'thriller', 'drama', 'documentary'];
-  var angryGenres = ['action', 'crime', 'thriller'];
+    var sadGenres = ['drama', 'tv movie', 'family'];
+    var happyGenres = ['adventure', 'animation', 'comedy', 'family', 'fantasy'];
+    var somberGenres = ['crime', 'documentary', 'drama', 'mystery', 'thriller', 'war'];
+    var sappyGenres = ['romance', 'tv movie', 'comedy', 'drama', 'family'];
+    var scaredGenres = ['horror', 'mystery', 'thriller', 'drama', 'documentary'];
+    var angryGenres = ['action', 'crime', 'thriller'];
 
-  // will populate the html elements and return html
-  function showRecs() {
+    // will populate the html elements and return html
+    function showRecs(movieList) {
+        $('.mood-choice').hide();
+        var movieResult = $('.template').clone();
+        movieResult.()
 
-  }
-
-  // will query the api and receive the object and print
-  // the html
-  function getMoodRecs(genre) {
-    $.ajax({
-      method: 'GET',
-      url: '',
-      dataType: 'json',
-      data: {
-
-      }
-    }).done(function (mood) {
-
-      showRecs();
-    });
-  };
-
-  function moodGenres(mood) {
-    var genre;
-    if (mood === 'sad') {
-      genre = 'drama';
-    } else if (mood === 'happy') {
-      genre = 'animation';
-    } else if (mood === 'somber') {
-      genre = 'crime';
-    } else if (mood === 'sappy') {
-      genre = 'romance';
-    } else if (mood === 'scared') {
-      genre = 'horror';
-    } else {
-      genre = 'action';
     }
-    getMoodRecs(genre);
-  }
 
-  // will take whatever tags and query the api and receive the
-  // object and print the html
-  function getClassicRecs(tags) {
-    // showRecs();
-  }
+    // will query the api and receive the object and print
+    // the html
+    function getMoodRecs(genre) {
+        $.ajax({
+            method: 'GET',
+            url: 'http://api.themoviedb.org/3/discover/movie',
+            dataType: 'jsonp',
+            data: {
+                api_key: 'b036e4fb522494bd075e0c895ed017a0',
+                with_genres: genre
+            }
+        }).done(function (result) {
+            console.log(result);
+            showRecs(result);
+        });
+    };
 
-  // listen to button click for which method
-  $('.mood-button').on('click', function () {
-    event.preventDefault();
-    $('.splash-page').hide();
-    $('.mood-section').show();
-  });
+    function moodGenres(mood) {
+        var genre;
+        if (mood === 'sad') {
+            genre = 18;
+        } else if (mood === 'happy') {
+            genre = 16;
+        } else if (mood === 'somber') {
+            genre = 80;
+        } else if (mood === 'sappy') {
+            genre = 10749;
+        } else if (mood === 'scared') {
+            genre = 27;
+        } else {
+            genre = 28;
+        }
+        getMoodRecs(genre);
+    }
 
-  $('.classic-button').on('click', function () {
-    event.preventDefault();
-    $('.splash-page').hide();
-    $('.classic-section').show();
-  });
+    // will take whatever tags and query the api and receive the
+    // object and print the html
+    function getClassicRecs(tags) {
+        // showRecs();
+    }
 
-  // listen for Mood dropdown
-  $('.mood-choice').change(function () {
-    $('.mood-recs').empty();
-    var selectedMood = $(this).find(':selected').val();
-    moodGenres(selectedMood);
-  });
+    // listen to button click for which method
+    $('.mood-button').on('click', function () {
+        event.preventDefault();
+        $('.splash-page').hide();
+        $('.mood-section').show();
+    });
 
-  // listen for Classic Search submit button
-  $('.search-submit').on('click', function () {
-    event.preventDefault();
-    $('.classic-section').empty();
-    var searchQuery = $(this).val();
-    // getClassicRecs(searchQuery);
-  });
+    $('.classic-button').on('click', function () {
+        event.preventDefault();
+        $('.splash-page').hide();
+        $('.classic-section').show();
+    });
+
+    // listen for Mood dropdown
+    $('.mood-choice').change(function () {
+        $('.mood-recs').empty();
+        var selectedMood = $(this).find(':selected').val();
+        moodGenres(selectedMood);
+    });
+
+    // listen for Classic Search submit button
+    $('.search-submit').on('click', function () {
+        event.preventDefault();
+        $('.classic-section').empty();
+        var searchQuery = $(this).val();
+        // getClassicRecs(searchQuery);
+    });
 });
 
 
